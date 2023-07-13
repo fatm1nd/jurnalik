4bc06ebb6725b5e46e99b675a54d8ac2a62ff108
exemplerie <v.kharina@innopolis.university>
2023-07-13 19:36:18 +0300

    Update init_db.sql

--- backend/db_init_sctipt/init_db.sql
+++ backend/db_init_sctipt/init_db.sql
@@ -13,7 +13,7 @@ CREATE TABLE IF NOT EXISTS raw_posts(
 );
 
 CREATE TABLE IF NOT EXISTS prepared_posts(
-    post_id INT,
+    post_id VARCHAR,
     user_id INT,
     group_id BIGINT,
     category VARCHAR,
@@ -22,19 +22,20 @@ CREATE TABLE IF NOT EXISTS prepared_posts(
 
 CREATE TABLE IF NOT EXISTS items(
     item VARCHAR PRIMARY KEY,
-    post_id INT,
+    post_id VARCHAR,
     type VARCHAR
 );
 
 CREATE TABLE IF NOT EXISTS groups_and_channels(
     group_id INT PRIMARY KEY,
-    group_name varchar,
-    picture varchar,
-    source varchar
+    group_name VARCHAR,
+    picture VARCHAR,
+    source VARCHAR
 );
 
 CREATE TABLE IF NOT EXISTS telegram_channels(
-    group_id INT,
+    group_id BIGINT,
+    username VARCHAR,
     user_id INT
 );
 
