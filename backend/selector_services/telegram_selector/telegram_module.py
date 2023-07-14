088c72240878d2ac04b66140e7c786a06b84071c
exemplerie <v.kharina@innopolis.university>
2023-07-14 12:04:11 +0300

    Update telegram_module.py

--- backend/selector_services/telegram_selector/telegram_module.py
+++ backend/selector_services/telegram_selector/telegram_module.py
@@ -4,9 +4,9 @@ import get_posts
 
 def handle_one(user_id):
     channels = db.get_channels(user_id)
-    for channel_id in channels:
-        posts = get_posts.collect_posts(channel_id)
-        db.write_posts(user_id, channel_id, posts)
+    for channel_username in channels:
+        posts = get_posts.collect_posts(channel_username)
+        db.write_posts(user_id, channel_username, posts)
 
 
 def handle_all():
